61d6b5e72466e81151a19d4ab4f93c8f4d7b6ab5
sush <sushanthgangishetti@gmail.com>
2025-10-30 23:07:36 +0530

    Initial commit

---
+++ src/main/java/com/aizen/souske/SouskeApplication.java
@@ -0,0 +1,13 @@
+package com.aizen.souske;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SouskeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SouskeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aizen/souske/SouskeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aizen.souske;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SouskeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
